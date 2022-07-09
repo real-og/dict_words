149ea15226fc78ef28f89d6f8703b723e37d9c23
Evgeny Grakovich <grakovich-e@yandex.by>
2022-07-10 02:05:53 +0300

    add parser that returns set of words by url

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import string
+import re
 
-#url = 'https://genius.com/Kendrick-lamar-dna-lyrics'
+# url = 'https://genius.com/Kendrick-lamar-dna-lyrics'
 url = 'https://genius.com/Slowthai-adhd-lyrics'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 
-lines = soup.findAll(class_="Lyrics__Container-sc-1ynbvzw-6 YYrds")
+words = list()
+uniq_words = set()
+lyrics_blocks = soup.findAll(class_="Lyrics__Container-sc-1ynbvzw-6 YYrds")
 
-for i in lines:
-    s = i.get_text('\n')
-    print(s, len(s))
+for block in lyrics_blocks:
+    uniq_words |= set(re.findall('[’a-zа-яё\']+', block.get_text('\n').lower()))
+
+for block in lyrics_blocks:
+    words = words + re.findall('[’a-zа-яё\']+', block.get_text('\n').lower())
+
+# problem with connected - like "dec--"
+# words = [word.strip(string.punctuation).lower() for word in lyrics.split()]
+
+
+
