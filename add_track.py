09b2e77a21a7644a0143b839e296ba541459a539
Evgeny Grakovich <grakovich-e@yandex.by>
2022-07-30 07:30:02 +0300

    add_track is add track we need separate logic in bot

--- add_track.py
+++ add_track.py
@@ -8,3 +8,9 @@ def add_track(url):
     return len(words)
     
 
+if __name__ == '__main__':    
+    while True:
+        url = input("Введите адрес на джиниус: ")
+        count_before = get_words_count()
+        print("В песне " + str(add_track(url)) + " слов")
+        print("Добавлено " + str(get_words_count() - count_before))
