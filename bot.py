a76b4c0d640688733b874c11c237f8e964b80ff6
Evgeny Grakovich <grakovich-e@yandex.by>
2022-07-31 20:12:30 +0300

    all the commands are implemented

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-import time
 import os
 import logging
 
-from keyboards import menu_kb
+from keyboards import menu_kb, choice_kb, big_start_kb
 
-from add_track import add_track
 from LyricsParser import LyricsParser
 
 
@@ -32,31 +30,7 @@ class State(StatesGroup):
     add_w = State()
     check_t = State()
     check_w = State()
-
-    command = State()
-    check = State()
-
-
-@dp.message_handler(state=State.command)
-async def execute_command(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    command = data['name']
-
-    if command == '/add_song':
-        start = time.time()
-        count_before = db.get_words_count()
-        lenn = add_track(message.text.lower())
-        count_after = db.get_words_count() - count_before
-        await message.answer("Добавлено " + str(count_after) + " новых слов из " + str(lenn) + " возможных за " + str('%.3f' % (time.time() - start)) + " сек", reply_markup=menu_kb)
-        await state.finish()
-
-   
-
-    if command == '/check_song':
-        url = message.text.lower()
-        words = LyricsParser(url).get_word_list()
-        await State.check.set()
-        await state.update_data(words)
+    checking = State()
 
 
 @dp.message_handler(commands=['start'])
@@ -67,13 +41,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(regexp='Команды')
 @dp.message_handler(commands=['commands','help'])
 async def send_commands(message: types.Message):
-    await message.reply("""/add_song - Добавить песню целиком\n/add_word - Добавить слово\n/check_song - Добавить песню проверяя каждое слово\n/check_word - Проверить, если ли слово\n/count - Количество слов в базе\n/commands - Команды""", reply_markup=menu_kb)
-
-
-
-
-
-
+    await message.reply("""/commands - Команды\n/add_song - Добавить песню целиком\n/add_word - Добавить слово\n/check_song - Добавить песню проверяя каждое слово\n/check_word - Проверить, если ли слово\n/count - Количество слов в базе""", reply_markup=menu_kb)
 
 
 @dp.message_handler(regexp='Добавить трек')
@@ -99,12 +67,6 @@ async def add_track(message: types.Message, state: FSMContext):
     await state.finish()
 
 
-
-
-
-
-
-
 @dp.message_handler(regexp='Добавить слово')
 @dp.message_handler(commands=['add_word'])
 async def add_word(message: types.Message):
@@ -122,7 +84,7 @@ async def execute_command(message: types.Message, state: FSMContext):
     await state.finish()
 
     
-@dp.message_handler(regexp='Есть ли слово')
+@dp.message_handler(regexp='Проверить слово')
 @dp.message_handler(commands=['check_word'])
 async def check_wordd(message: types.Message):
     await message.answer("Введите слово:")
@@ -138,18 +100,93 @@ async def execute_command(message: types.Message, state: FSMContext):
     await state.finish()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @dp.message_handler(regexp='Проверить трек')
 @dp.message_handler(commands=['check_song'])
-async def check_song(message: types.Message, state: FSMContext):
-    await message.answer("Введите ссылку на genius:")
-    await State.command.set()
-    await state.update_data(name=message.text)
+async def add_song(message: types.Message):
+    await message.answer("Введите сcылку на Genius:")
+    await State.check_t.set()
+
+@dp.message_handler(state=State.check_t)
+async def check_track(message: types.Message, state: FSMContext):
+    url = message.text.lower()
+    words = LyricsParser(url).get_word_list()
+    await message.answer("Всего в треке " + str(len(words)) + " слов.")
+    new_words = list()
+    for word in words:
+        if not db.check_word(word):
+            new_words.append(word)
+    if len(new_words) == 0:
+        await message.answer("В этом треке все слова знакомы", reply_markup=menu_kb)
+        await state.finish()
+    await message.answer("Из них незнакомых " + str(len(new_words)) + "\nНажмите Начать, чтобы начать добавление по слову", reply_markup=big_start_kb)
+    await state.update_data(words_to_check=new_words)
+    await state.update_data(iter=0)
+    await state.update_data(words_to_learn=list())
+    await State.checking.set()
+
+@dp.message_handler(state=State.checking)
+async def check_(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    i = data['iter']
+    new_words = data['words_to_check']
+    to_learn_words = data['words_to_learn']
+    
+    if message.text.lower() == 'начать':
+        pass
+    elif message.text.lower() == 'да':
+        db.add_word(new_words[i-1])
+    elif message.text.lower() == 'нет':
+        to_learn_words.append(new_words[i-1])
+
+    if i < len(new_words):
+        await message.answer(new_words[i], reply_markup=choice_kb)
+        i += 1
+    else:
+        await message.answer('Проверка окончена, слова которые нужно выучить:')
+        await message.answer(str(to_learn_words), reply_markup=menu_kb)
+        await state.finish()
+        return
+    await state.update_data(words_to_learn=to_learn_words, iter=i)
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
 
 
 @dp.message_handler(regexp='Количество слов')
 @dp.message_handler(commands=['count'])
 async def count(message: types.Message, state: FSMContext):
     await message.answer("В базе " + str(db.get_words_count()) + " слов.", reply_markup=menu_kb)
+
 
 if __name__ == '__main__':
     print("Starting")
--- keyboards.py
+++ keyboards.py
@@ -1,3 +1,4 @@
+from secrets import choice
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
@@ -11,10 +12,25 @@ check_w = KeyboardButton('Проверить слово')
 check_t = KeyboardButton('Проверить трек')
 count = KeyboardButton('Количество слов')
 
-
 menu_kb.insert(commands)
 menu_kb.insert(add_t)
 menu_kb.insert(check_t)
 menu_kb.insert(count)
 menu_kb.insert(add_w)
 menu_kb.insert(check_w)
+
+
+choice_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+yes = KeyboardButton('Да')
+no = KeyboardButton('Нет')
+
+choice_kb.insert(yes)
+choice_kb.insert(no)
+
+
+big_start_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+start = KeyboardButton('Начать')
+
+big_start_kb.insert(start)
