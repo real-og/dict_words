bd337ad7eb6b9b7d9c4182ad74c4a2e926c54334
Evgeny Grakovich <grakovich-e@yandex.by>
2022-08-15 12:11:39 +0300

    move initwirds to a dir

--- initializer.py
+++ initializer.py
@@ -3,7 +3,7 @@ from db import add_word_to_user
 # Adds words to a user from file like a1.txt, or a2.txt
 # Returns the number of added words
 def init_user_dict(id_tg, filename):
-    with open(filename, 'r') as f:
+    with open('words_to_init/' + filename, 'r') as f:
         words = f.read().split()
         for word in words:
             add_word_to_user(id_tg, word)
